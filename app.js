d5d51ee630d9e81f1c279d68baf79be93d7b602a
Rajshree Chaurasia <rajshreevc2018@gmail.com>
2026-04-04 23:53:25 +0530

    Simplify server response in app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Hello from Node App using Docker + Jenkins</h1>");
+http.createServer((req, res) => {
+    res.write("Hello from Node App");
     res.end();
-});
-
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+}).listen(3000);
